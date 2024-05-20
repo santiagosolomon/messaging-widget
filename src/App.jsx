2f0d070cc291da7883ee30e927b5c1a6ca3cc814
MonnNster4 <monsantiago09@gmail.com>
2024-05-21 04:57:53 +0800

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,19 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div className="h-screen  ">
-      <div className="h-full border rounded-xl shadow">
+    <>
+    <div className="h-screen">
+      <div className="  border rounded-xl shadow">
         {/* <div className="flex justify-between p-4 items-center h-24 bg-gray-400 max-w-full w-full">
-          <div className="flex justify-center max-w-[7xl] w-full font- ">Help Bot</div>
+          <div className="flex justify-center max-w-[7xl] w-full font- ">
+            Help Bot
+          </div>
           <IoMdClose className="text-lg" />
         </div> */}
-        <div className="h-[40rem] bg-gray-100">
+        <div className="h-[47rem] bg-gray-100 overflow-auto">
           <div className=" flex flex-row p-8 gap-4 ">
-            <div className="  max-w-[800px] border rounded-b-lg rounded-tr-lg h-full min-h-[100px] w-auto  bg-white p-2 mx-2 break-words ">
+            <div className="rounded-2xl bg-white w-full max-w-8 h-8 max-[575px]:w-[85rem] "></div>
+            <div className="  max-w-[500px] border rounded-b-lg rounded-tr-lg h-full min-h-[100px] w-auto  bg-white p-2 mx-2 break-words  ">
               <span>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Eveniet laborum, veniam corrupti eligendi repellat veritatis in
@@ -33,34 +37,59 @@ function App() {
                 illum et officia cupiditate, error nemo sint, aliquam
                 perspiciatis sunt quo molestiae reprehenderit eum modi neque
                 fugiat, rem animi labore? Eveniet obcaecati iusto at aliquam,
-                minima vero commodi nulla facere odio voluptatem porro adipisci
-                nobis, autem qui dolorem architecto blanditiis dolores earum
-                quod sequi voluptates. Placeat dignissimos et perferendis
-                adipisci? Cupiditate ab laborum at suscipit voluptates! Error in
-                fugit autem animi pariatur tempore ducimus, accusantium
-                molestias accusamus exercitationem ratione placeat asperiores
-                quam. Rerum nobis commodi, possimus explicabo quasi molestiae,
-                ea quod mollitia cumque illasdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdjsahdjkashkdjhsajkdhkajshdkjsajhaghskjahgkjsahgkjashgjkhsajkghasjkhgjksahgjkashgksajhgakjshgjkashgjkashkgjashdkjshajkdhsajkdhjksahdjksahdasjkhdkjashdjkashdkjsahdksasjdhsakjhdkjsahdjkashdjkashdkjahskdjdsad
               </span>
             </div>
           </div>
-          <div className="w-full flex justify-end p-4 max-w-[700px] ">
-            <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full max-w-[15rem] p-2 bg-blue-500 text-white ">
-              dsasssssssssssssssddsads
+          <div className="w-full flex justify-end p-4 max-w-[7000px] ">
+            <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full p-2 bg-blue-500 text-white max-w-[30rem] break-words ">
+              dsasssssssssssssssddsads placeat non dignissimos repellat fugit?
+              Asperiores rerum enim nobis, autem qui dolorem architecto
+              blanditiis dolores earum quod sequi voluptates. Placeat
+              dignissimos et perferendis adipisci? Cupiditate ab laborum at
+              suscipit voluptates! Error in fugit autem animi pariatur tempore
+              ducimus, accusantium molestias accusamus exercitationem ratione
+              placeat asperiores quam. Rerum nobis commodi, possimus explicabo
+              quasi molestiae, placeat non dignissimos repellat fugit?
+              Asperiores rerum enim
+              dsadsadsadsadsadsaddsaddsdadsadsdsadsadsdsdsadsdsadsadsadsadsdd
+              dsadsadddsadsaadadsadadadadasadsadsadssadsadsadsadsxxxxxxxxxxxxdsad
+              dsasdsadsadsadsadsadsaaaaaaaaaaaaaaasdsdssdsdsdsdsddsadsasasasasasasasasa
+              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
+              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
+              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
+            </div>
+          </div>
+          <div className=" flex flex-row p-8 gap-4 ">
+            <div className="rounded-2xl bg-white w-full max-w-8 h-8  "></div>
+            <div className="  max-w-[500px] border rounded-b-lg rounded-tr-lg h-full min-h-[100px] w-auto  bg-white p-2 mx-2 break-words  ">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Eveniet laborum, veniam corrupti eligendi repellat veritatis in
+                doloremque. Accusantium fuga suscipit aperiam minima porro saepe
+                ea perferendis sed at minus sint, placeat deleniti, blanditiis
+               
+              </span>
+            </div>
+          </div>
+          <div className="w-full flex justify-end p-4 max-w-[7000px] ">
+            <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full p-2 bg-blue-500 text-white max-w-[30rem] break-words ">
+              dsasssssssssssssssddsads placeat non dignissimos repellat fugit?
+           
+            
             </div>
           </div>
         </div>
         <form>
-          <div class="flex items-center px-3 py-2    ">
+          <div class="flex items-center px-3 pb-2 pt-4    ">
             <textarea
               id="chat"
-              rows="3"
+              rows="7"
               class="mx-4  w-full text-sm text-gray-900 bg-white outline-none  h-auto  "
               placeholder="Your message..."
             ></textarea>
             <button
               type="submit"
-              class="inline-flex justify-center p-2  cursor-pointer "
+              class="inline-flex justify-center p-2  cursor-pointer hover:text-slate-500 "
             >
               <svg
                 class="w-5 h-5 rotate-90 rtl:-rotate-90 mb-10"
@@ -77,6 +106,7 @@ function App() {
         </form>
       </div>
     </div>
+    </>
   );
 }
 
