2d575d96ef65c55de130ae7994c5b1206f9467de
Solomon Santiago <monsantiag09@gmail.com>
2024-05-22 12:32:43 +0800

    hover ui, h-fit

--- src/App.jsx
+++ src/App.jsx
@@ -5,107 +5,64 @@ import "./App.css";
 import { IoMdClose } from "react-icons/io";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  //  const [text, setText] = useState("dsadsadadsadsdsadsaddsadsaddaaaa");
 
   return (
     <>
-    <div className="h-screen">
-      <div className="  border rounded-xl shadow">
-        <div className="flex justify-between p-4 items-center h-24 bg-gray-400 max-w-full w-full">
-          <div className="flex justify-center max-w-[7xl] w-full font- ">
-            Help Bot
-          </div>
-          <IoMdClose className="text-lg" />
-        </div>
-        <div className="h-[47rem] bg-gray-100 overflow-auto">
-          <div className=" flex flex-row p-8 gap-4 ">
-            <div className="rounded-2xl bg-white w-full max-w-8 h-8 max-[575px]:w-[85rem] "></div>
-            <div className="  max-w-[500px] border rounded-b-lg rounded-tr-lg h-full min-h-[100px] w-auto  bg-white p-2 mx-2 break-words  ">
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Eveniet laborum, veniam corrupti eligendi repellat veritatis in
-                doloremque. Accusantium fuga suscipit aperiam minima porro saepe
-                ea perferendis sed at minus sint, placeat deleniti, blanditiis
-                atque magnam sequi laborum repellat, dolore cupiditate! Debitis,
-                illo? Voluptas blanditiis officiis, magnam natus aspernatur
-                accusantium, quod ducimus a dolor atque cupiditate quisquam
-                ipsum ab praesentium sed incidunt rem consequatur. Dicta vero
-                totam nisi. Natus perferendis et iusto nulla facere voluptatem
-                earum quaerat. Eligendi accusantium adipisci incidunt
-                cupiditate, ipsam voluptatibus corporis sed excepturi similique,
-                placeat non dignissimos repellat fugit? Asperiores rerum enim
-                illum et officia cupiditate, error nemo sint, aliquam
-                perspiciatis sunt quo molestiae reprehenderit eum modi neque
-                fugiat, rem animi labore? Eveniet obcaecati iusto at aliquam,
-              </span>
+      <div className="h-screen">
+        <div className="  border rounded-xl shadow">
+          <div className="flex justify-between p-4 items-center h-24 bg-gray-400 max-w-full w-full">
+            <div className="flex justify-center max-w-[7xl] w-full  ">
+              Help Bot
             </div>
           </div>
-          <div className="w-full flex justify-end p-4 max-w-[7000px] ">
-            <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full p-2 bg-blue-500 text-white max-w-[30rem] break-words ">
-              dsasssssssssssssssddsads placeat non dignissimos repellat fugit?
-              Asperiores rerum enim nobis, autem qui dolorem architecto
-              blanditiis dolores earum quod sequi voluptates. Placeat
-              dignissimos et perferendis adipisci? Cupiditate ab laborum at
-              suscipit voluptates! Error in fugit autem animi pariatur tempore
-              ducimus, accusantium molestias accusamus exercitationem ratione
-              placeat asperiores quam. Rerum nobis commodi, possimus explicabo
-              quasi molestiae, placeat non dignissimos repellat fugit?
-              Asperiores rerum enim
-              dsadsadsadsadsadsaddsaddsdadsadsdsadsadsdsdsadsdsadsadsadsadsdd
-              dsadsadddsadsaadadsadadadadasadsadsadssadsadsadsadsxxxxxxxxxxxxdsad
-              dsasdsadsadsadsadsadsaaaaaaaaaaaaaaasdsdssdsdsdsdsddsadsasasasasasasasasa
-              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
-              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
-              dsadsadadsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddsddddddddddddddddddddddddddddddddddddddddddddddddddd
+          <div className="h-[31.95rem] bg-gray-100 overflow-auto">
+            <div className=" flex flex-row p-8  ">
+              <div className="rounded-2xl bg-white w-full max-w-8 h-8 max-[575px]:w-[85rem] "></div>
+              <div className="flex flex-col w-full">
+                <div className=" float-left inline-block  whitespace-nowrap overflow-hidden   border rounded-b-lg rounded-tr-lg bg-white p-2 mx-2 break-words  ">
+                 dasdssadsdsadsd
+                </div>
+                <div className="w-full flex justify-end p-4 max-w-[7000px] ">
+                  <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full p-2 bg-blue-500 text-white max-w-[30rem] break-words ">
+                    dsasssssssssssssssddsads placeat non dignissimos repellat
+                    fugit? Asperiores rerum enim nobis, autem qui dolorem
+                    architecto blanditiis dolores earum quod sequi voluptates.
+                    Placeat dignissimos et perferendis adipisci? Cupiditate ab
+                    laborum at suscipit voluptates! Error in fugit autem animi
+                    pariatur tempore ducimus, accusantium molestias accusamus
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className=" flex flex-row p-8 gap-4 ">
-            <div className="rounded-2xl bg-white w-full max-w-8 h-8  "></div>
-            <div className="  max-w-[500px] border rounded-b-lg rounded-tr-lg h-full min-h-[100px] w-auto  bg-white p-2 mx-2 break-words  ">
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Eveniet laborum, veniam corrupti eligendi repellat veritatis in
-                doloremque. Accusantium fuga suscipit aperiam minima porro saepe
-                ea perferendis sed at minus sint, placeat deleniti, blanditiis
-               
-              </span>
-            </div>
-          </div>
-          <div className="w-full flex justify-end p-4 max-w-[7000px] ">
-            <div className=" border rounded-b-lg rounded-tl-lg h-full max-h-auto w-full p-2 bg-blue-500 text-white max-w-[30rem] break-words ">
-              dsasssssssssssssssddsads placeat non dignissimos repellat fugit?
-           
-            
-            </div>
-          </div>
-        </div>
-        <form>
-          <div class="flex items-center px-3 pb-2 pt-4    ">
-            <textarea
-              id="chat"
-              rows="7"
-              class="mx-4  w-full text-sm text-gray-900 bg-white outline-none  h-auto  "
-              placeholder="Your message..."
-            ></textarea>
-            <button
-              type="submit"
-              class="inline-flex justify-center p-2  cursor-pointer hover:text-slate-500 "
-            >
-              <svg
-                class="w-5 h-5 rotate-90 rtl:-rotate-90 mb-10"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 20"
+          <form>
+            <div class="flex items-center px-3 pb-2 pt-4    ">
+              <textarea
+                id="chat"
+                rows="7"
+                class="mx-4  w-full text-sm text-gray-900 bg-white outline-none  h-auto  "
+                placeholder="Your message..."
+              ></textarea>
+              <button
+                type="submit"
+                class="inline-flex justify-center p-2  cursor-pointer hover:text-slate-500 "
               >
-                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-              </svg>
-              <span class="sr-only">Send message</span>
-            </button>
-          </div>
-        </form>
+                <svg
+                  class="w-5 h-5 rotate-90 rtl:-rotate-90 mb-10"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 20"
+                >
+                  <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                </svg>
+                <span class="sr-only">Send message</span>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 }
